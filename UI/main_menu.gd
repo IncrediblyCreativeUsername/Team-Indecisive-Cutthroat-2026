ac72586700f3baa32aa0e7c6767b62f55ae1367d
SparTimm <66032192+SparTimm@users.noreply.github.com>
2026-04-10 20:23:22 -0400

    Placeholder assets
    Added placeholder assets and updated main menu

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -7,3 +7,6 @@ func _on_play_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), (75-value))
