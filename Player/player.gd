129342d1ea5cd12fb47c6a6acf2475f2a68dc3f1
Freddie Hatch <freddiehatch@autoreg-6802278.dyn.wpi.edu>
2026-04-10 20:54:05 -0400

    Jump Movement, Air Time, Coyote Time

--- Player/player.gd
+++ Player/player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
-@export var speed : int = 200
-@export var jumpForce : int = 1000
+@export var speed : int = 700
+@export var jumpForce : int = 2000
 @export var gravity : int = 100
+@export var coyoteTime : int = 15
 
+var coyoteTimer = 0
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	#X movement
 	velocity.x = 0
 	
 	if Input.is_action_pressed("MOVE_RIGHT"):
@@ -15,10 +18,30 @@ func _physics_process(delta: float) -> void:
 	
 	velocity.x *= speed
 	
-	if Input.is_action_just_pressed("JUMP"):
-		if is_on_floor():
+	#coyote time resets while on floor
+	if is_on_floor():
+		coyoteTimer = coyoteTime
+	else:
+		coyoteTimer -= 1
+	
+	#Y movement
+	if Input.is_action_pressed("JUMP"):
+		#base jump force
+		if is_on_floor() or coyoteTimer > 0:
+			coyoteTimer = 0
+			velocity.y = 0
 			velocity.y -= jumpForce
+	else:
+		#shorter jump if not held
+		if velocity.y < 0:
+			velocity.y /= 1.3
+	
+	#air time
+	if velocity.y < 0:
+		if velocity.y > 100:
+			velocity.y -= gravity * 0.8
 	
 	velocity.y += gravity
 	
+	#move according to velocity and delta
 	move_and_slide()
