a0b28c9342615ca3df918c2d370f1800b3f08628
SparTimm <66032192+SparTimm@users.noreply.github.com>
2026-04-10 20:42:12 -0400

    Hat switching and play button
    Added menu buttons for changing hat, removed test_scene, "Play" now opens game, main menu is now main scene

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+@onready var hatLabel := $HatNumLabel
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file(Globals.gameScene)
@@ -14,3 +14,17 @@ func _on_volume_slider_value_changed(value: float) -> void:
 	else:
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
 		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), (value-75)/5)
+
+func _on_prev_hat_pressed() -> void:
+	if Globals.playerHatNum == 0:
+		Globals.playerHatNum = Globals.hatCount-1
+	else:
+		Globals.playerHatNum -= 1
+	hatLabel.text = "Hat:\n#" + str(Globals.playerHatNum)
+
+func _on_next_hat_pressed() -> void:
+	if Globals.playerHatNum == Globals.hatCount-1:
+		Globals.playerHatNum = 0
+	else:
+		Globals.playerHatNum += 1
+	hatLabel.text = "Hat:\n#" + str(Globals.playerHatNum)
