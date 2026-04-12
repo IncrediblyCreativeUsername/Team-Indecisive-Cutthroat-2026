9cb0f94774aa1b2b2602024dade3e8ed6089dc79
SparTimm <timmah514@gmail.com>
2026-04-11 22:15:57 -0400

    Delta movement, grapple rework
    Multiplied movement values by delta time properly, rewrote grapple to properly function, added camera smoothing

--- Player/player.gd
+++ Player/player.gd
@@ -4,86 +4,83 @@ extends CharacterBody2D
 @export var jumpForce : int = 2000
 @export var gravity : int = 100
 @export var coyoteTime : int = 15
-@export var grappleTime : int = 30
 @export var friction : int = 500
-@export var tongueForce : float = 10.0
 
 var coyoteTimer = 0
-var grappleTimer = 0
+var grapplePoint = Vector2.ZERO
 @export var tongueExtending := false
 @onready var tongueAnimator := $Tongue/TongueAnim
 @onready var tonguePivot := $Tongue
 @onready var tongueTip := $Tongue/Area2D
+@onready var tongueCast := $Tongue/RayCast2D
 var grabbedObject
 var isGrappling : bool = false
 
 func _physics_process(_delta: float) -> void:
-	#friction
-	if velocity.x > 0:
-		velocity.x = clamp(velocity.x-friction, 0, velocity.x-friction)
-	if velocity.x < 0:
-		velocity.x = clamp(velocity.x+friction, velocity.x+friction, 0)
-	
-	#X movement
-	
-	if Input.is_action_pressed("MOVE_RIGHT") && !tongueExtending && velocity.x < speed:
-		velocity.x += speed
-	if Input.is_action_pressed("MOVE_LEFT") && !tongueExtending && velocity.x > -speed:
-		velocity.x -= speed
-	
-	#coyote time resets while on floor
-	if is_on_floor():
-		coyoteTimer = coyoteTime
-	else:
-		coyoteTimer -= 1
-	
-	#Y movement
-	if Input.is_action_pressed("JUMP") && !tongueExtending && !isGrappling:
-		#base jump force
-		if is_on_floor() or coyoteTimer > 0:
-			coyoteTimer = 0
-			velocity.y = 0
-			velocity.y -= jumpForce
-	else:
-		#shorter jump if not held
-		if velocity.y < 0:
-			velocity.y /= 1.3
-	
-	#air time
-	if velocity.y < 0:
-		if velocity.y > 100:
-			velocity.y -= gravity * 0.8
-	
-	velocity.y += gravity
-	
-	#shoot out tongue
-	if !tongueExtending && Input.is_action_just_pressed("GRAPPLE"):
-		tonguePivot.look_at(get_global_mouse_position())
-		tongueAnimator.play("Extend")
-	
-	#move grabbed object
-	if grabbedObject != null && tongueExtending:
-		grabbedObject.global_position = $Tongue/Area2D.global_position
-	else:
-		grabbedObject = null
-	
+	#grapple
 	if isGrappling:
-		manageGrapple()
+		global_position = grapplePoint - (tongueTip.position+Vector2(64+9,0)).rotated(tonguePivot.rotation)
 	
-	#move according to velocity and delta
-	move_and_slide()
-
-func manageGrapple():
-	if grappleTimer <= 0:
-		isGrappling = false
+	#movement
 	else:
-		grappleTimer -= 1
-	velocity += Vector2((tongueForce)/get_process_delta_time(), 0).rotated(tonguePivot.rotation)
+		#friction
+		if velocity.x > 0:
+			velocity.x = max(0, (velocity.x-friction)*60*_delta)
+		if velocity.x < 0:
+			velocity.x = min((velocity.x+friction)*60*_delta, 0)
+		
+		#X movement
+		
+		if Input.is_action_pressed("MOVE_RIGHT") && !tongueExtending && velocity.x < speed:
+			velocity.x += speed*60*_delta
+		if Input.is_action_pressed("MOVE_LEFT") && !tongueExtending && velocity.x > -speed:
+			velocity.x -= speed*60*_delta
+		
+		#coyote time resets while on floor
+		if is_on_floor():
+			coyoteTimer = coyoteTime
+		else:
+			coyoteTimer -= 1
+		
+		#Y movement
+		if Input.is_action_pressed("JUMP") && !tongueExtending && !isGrappling:
+			#base jump force
+			if is_on_floor() or coyoteTimer > 0:
+				coyoteTimer = 0
+				velocity.y = 0
+				velocity.y -= jumpForce
+		else:
+			#shorter jump if not held
+			if velocity.y < 0:
+				velocity.y /= 1.3
+		
+		#air time
+		if velocity.y < 0:
+			if velocity.y > 100:
+				velocity.y -= gravity * 0.8
+		
+		velocity.y += gravity
+		
+		#shoot out tongue
+		if !tongueExtending && Input.is_action_just_pressed("GRAPPLE"):
+			tonguePivot.look_at(get_global_mouse_position())
+			tongueAnimator.play("Extend")
+		
+		#move grabbed object
+		if grabbedObject != null && tongueExtending:
+			grabbedObject.global_position = $Tongue/Area2D.global_position
+		else:
+			grabbedObject = null
+		
+		#move according to velocity and delta
+		move_and_slide()
 	
 #tongue fully extends
 func _on_tongue_anim_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "Extend":
 		tongueAnimator.play("Retract")
+	if anim_name == "Retract":
+		isGrappling = false
 
 #tongue hit something
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -99,7 +96,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 			grabbedObject.grabbed(global_position)
 		#grapple otherwise
 		else:
-			velocity.y = 0
-			velocity = Vector2(tongueForce/get_process_delta_time(), 0).rotated(tonguePivot.rotation)
-			grappleTimer = grappleTime
+			velocity = Vector2.ZERO
+			#grapplePoint = tongueTip.global_position
+			grapplePoint = tongueCast.get_collision_point() + Vector2(64,0).rotated(tonguePivot.rotation)
 			isGrappling = true
