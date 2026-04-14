9bdddaaab95e0f226ebb48ca5be315514736d162
invinciblepig <115884514+invinciblepig@users.noreply.github.com>
2026-04-14 00:50:16 -0400

    Basic boss

---
+++ Boss/boss.gd
@@ -0,0 +1,66 @@
+extends CharacterBody2D
+
+@export var health : int = 10
+@export var gravity : int = 0
+@export var speed : int = 600
+@export var speedMod : int = 1
+@export var damage : int = 1
+@export var knockback : int = 2000
+@export var damageCooldownMax : int = 30
+
+@onready var player = Globals.player
+@onready var visibility := $VisibleOnScreenEnabler2D
+
+@onready var sprite = $Sprite2D
+@onready var bossbar = $Bossbar
+var lifetime : float = 0
+var damageCooldown = 0
+
+var spawnpoint : Vector2
+
+func _ready() -> void:
+	spawnpoint = global_position
+	bossbar.maxHealth = health
+	bossbar.health = health
+
+func _physics_process(_delta: float) -> void:
+	bossbar.health = health
+	if health <= 0:
+		queue_free()
+	elif health <= 5:
+		speedMod = 2
+	
+	if damageCooldown > 0:
+		damageCooldown -= 1
+	if damageCooldown <= 0 && $DamageArea.monitoring == false:
+		$DamageArea.set_deferred("monitoring",true)
+	
+	#only execute code if onscreen
+	if visibility.is_on_screen():
+		bossbar.visible = true
+		lifetime += _delta
+		velocity.y = cos(lifetime * speedMod * PI) * speed * speedMod
+		velocity.x = cos(lifetime * speedMod * PI * 0.25) * speed * speedMod
+		$spawn.global_position = spawnpoint
+		move_and_slide()
+
+func grabbed(_grabOrigin : Vector2):
+	damageCooldown = damageCooldownMax
+	$DamageArea.set_deferred("monitoring",false)
+	health -= 1
+	#wasGrabbed = true
+
+
+func _on_damage_area_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		Globals.hurt(damage)
+		$DamageArea.set_deferred("monitoring",false)
+		damageCooldown = damageCooldownMax
+		if velocity.x > 0:
+			body.velocity.x += knockback
+		else:
+			body.velocity.x -= knockback
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
+	bossbar.visible = false
---
+++ UI/bossbar.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+var maxHealth : int = 10
+var health : int = maxHealth
+
+@onready var healthbar := $Health
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	healthbar.anchor_right = 0.05 + 0.9 * health / maxHealth
