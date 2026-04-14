7b149125c5d497088ca157fd456192df47ffefbd
Freddie Hatch <freddiehatch@autoreg-6802278.dyn.wpi.edu>
2026-04-14 15:06:47 -0400

    Full Level (First One) and Tweaks

--- globals.gd
+++ globals.gd
@@ -1,7 +1,7 @@
 extends Node
 
-#var gameScene := "res://Game/Game.tscn"
-var gameScene := "res://Game/game2.tscn"
+var gameScene := "res://Game/Game.tscn"
+#var gameScene := "res://Game/game2.tscn"
 var mainMenuScene := "res://UI/MainMenu.tscn"
 var deathScene := "res://UI/GameEnd.tscn"
 var playerHatNum := 0
@@ -10,6 +10,8 @@ var hp := 3
 var hunger := 100.0
 var heldAnt := false
 var player
+var invincibilityFrames = 40
+var invincible = 0
 
 func resetValues():
 	hp = 3
@@ -17,11 +19,13 @@ func resetValues():
 	heldAnt = false
 
 func hurt(damage):
-	hp -= damage
-	if hp <= 0:
-		die()
-		#get_tree().reload_current_scene()
-		#resetValues()
+	if invincible <= 0:
+		invincible = invincibilityFrames
+		hp -= damage
+		if hp <= 0:
+			die()
+			#get_tree().reload_current_scene()
+			#resetValues()
 
 func die():
 	get_tree().change_scene_to_file(Globals.deathScene)
@@ -32,3 +36,5 @@ func _process(delta: float) -> void:
 		die()
 		#get_tree().reload_current_scene()
 		#resetValues()
+	if invincible > 0:
+		invincible -= 1
